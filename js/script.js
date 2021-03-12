e486152c4b91f8dc80a5197e7b65c0e58f84b2a8
B.C <benblog2000@gmail.com>
2021-03-12 09:30:10 +0100

    v0.8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
 var finalPrice;
+var coupons = [
+  'Ilmiosconto01',
+  'Ilmiosconto02',
+  'Ilmiosconto03'
+];
 var calculator = document.getElementById('calculate');
 calculator.addEventListener('click', function() {
 
@@ -15,26 +20,23 @@ calculator.addEventListener('click', function() {
     for (var i = 0; i < ingredients.length; i++) {
       var ingredient = ingredients[i];
       var trueIngredient = ingredient.checked;
-      var priceIngredient = parseInt(ingredient.dataset.price)
+      var priceIngredient;
 
       if (trueIngredient) {
+        priceIngredient = parseInt(ingredient.dataset.price);
         finalPrice += priceIngredient;
       }
 
     }
 
-    var coupons = [
-      'Ilmiosconto01',
-      'Ilmiosconto02',
-      'Ilmiosconto03'
-    ];
+
 
     var burgerCoupon = document.getElementById('burger-coupon').value;
+    var finded = false;
     for (var i = 0; i < coupons.length; i++) {
 
        var coupon = coupons[i];
 
-       var finded = false;
 
        if (coupon == burgerCoupon) {
 
